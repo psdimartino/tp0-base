ab8944f1b593ca6c2b86255c9ce4c370765586bf
Pablo S. Dimartino <pdimartino@fi.uba.ar>
2025-03-16 16:36:38 -0300

    Script para generar docker compose
    Se crean los scripts generar-compose.sh y docker-compose-generator.py los cuales crean el archivo de docker pedido. UN ejemplo de ejecucion es:
    
    ./generar-compose.sh docker-compose-test.yaml 5

---
+++ docker-compose-generator.py
@@ -0,0 +1,64 @@
+import sys
+import yaml
+
+
+def generate_docker_yaml(clients):
+    template = {
+        "name": "tp0",
+        "services": {
+            "server": {
+                "container_name": "server",
+                "image": "server:latest",
+                "entrypoint": "python3 /main.py",
+                "environment": [
+                    "PYTHONUNBUFFERED=1",
+                    "LOGGING_LEVEL=DEBUG"
+                ],
+                "networks": ["testing_net"]
+            }
+        },
+        "networks": {
+            "testing_net": {
+                "ipam": {
+                    "driver": "default",
+                    "config": [
+                        {"subnet": "172.25.125.0/24"}
+                    ]
+                }
+            }
+        }
+    }
+
+    for i in range(1, clients + 1):
+        client_name = f"client{i}"
+        template["services"][client_name] = {
+            "container_name": client_name,
+            "image": "client:latest",
+            "entrypoint": "/client",
+            "environment": [
+                f"CLI_ID={i}",
+                "CLI_LOG_LEVEL=DEBUG"
+            ],
+            "networks": ["testing_net"],
+            "depends_on": ["server"]
+        }
+
+    return template
+
+
+def dump_yaml_to_file(filename):
+    with open(filename, "w") as file:
+        yaml.dump(docker_compose_template, file)
+
+
+if __name__ == "__main__":
+    print("Se inicio el generador de docker compose")
+    if len(sys.argv) != 3:
+        print("Uso: python3 docker-compose-generator.py <output_file> <num_clients>")
+        sys.exit(1)
+
+    compose_filename = sys.argv[1]
+    num_clients = int(sys.argv[2])
+    print("Se va crear un docker compose de nombre", compose_filename, "para", num_clients, "clientes")
+    docker_compose_template = generate_docker_yaml(num_clients)
+    dump_yaml_to_file(compose_filename)
